fa481e2cbaa63226741e0fc664d272cfd1b11c94
Shahid Mumtaz <105332910+shahidmumtazaieng@users.noreply.github.com>
2026-04-18 09:14:06 +0500

    Delete frontend/dist/dev directory

--- frontend/dist/dev/static/chunks/app_page_tsx_0kcl1js._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0umzjjg._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0ezz.e5._.js","static/chunks/node_modules_zod_v4_1365uf0._.js","static/chunks/node_modules_axios_lib_0qhimsy._.js","static/chunks/node_modules_0p4f37c._.js"],
-    source: "dynamic"
-});
